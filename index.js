9c8b13994010fd5ebe77c8c28432a7df936e1199
Arkadeep <42931119+arka@users.noreply.github.com>
2019-01-01 01:19:26 +0530

    Twilio integraded

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ var fs = require('fs');
 var handlers = require('./lib/handlers');
 var helpers = require('./lib/helpers');
 
+//Change is needed
+helpers.sendTwilioSMS('8250058746','Hello!',function(err){
+  console.log("This was the error "+ err);
+})
 //Instantiate the HTTP server
 var httpServer = http.createServer(function (req, res) {
   unifiedServer(req, res);
