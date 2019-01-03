9a85fa295cc811e3f61e8cd53dabed6edb4c0028
Arkadeep <42931119+arka@users.noreply.github.com>
2019-01-04 03:11:24 +0530

    CLI support added

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ Primary file for API
 //Dependencies
 var server = require('./lib/server');
 var workers = require('./lib/workers');
-
+var cli = require('./lib/cli');
 //Declare the app
 var app = {}
 
 //Init function
 app.init = function () {
+  console.clear();
+  cli.verticalSpace(2);
+  cli.horizontalLine();
+  cli.centred("Admin Console");
+  cli.horizontalLine();
   //Start the server
   server.init();
 
   //Start the worker
   workers.init();
 
+  //Start the CLI, but make sure it starts last
+  setTimeout(function () {
+    cli.init();
+  }, 50);
 };
 
 //Execute
